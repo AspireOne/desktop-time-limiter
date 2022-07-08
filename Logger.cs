ba7b47c66618c677374c37f455e97bfeb7835853
Matěj Pešl <matejpesl1@gmail.com>
2022-07-09 00:24:40 +0200

    feat: max amount of runs shown in log

--- Logger.cs
+++ Logger.cs
@@ -10,12 +10,42 @@ namespace Wellbeing;
 public static class Logger
 {
     public static readonly string LogPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "log.txt");
-    private const int MaxLines = 400;
+    private const string RunSeparatorLine = "———————————————————";
+    private const int RunsShown = 10;
     private static readonly object FileLock = new();
 
     static Logger()
     {
-        File.AppendAllText(LogPath, $"\n\n———————————————————\n{DateTime.Now:F}\n");
+        File.AppendAllText(LogPath, $"\n\n{RunSeparatorLine}\n{DateTime.Now:F}\n");
+        FormatLog();
+    }
+
+    private static void FormatLog()
+    {
+        string[] logLines = File.ReadAllLines(LogPath);
+        int runs = logLines.Count(str => str == RunSeparatorLine);
+        if (runs <= RunsShown)
+            return;
+
+        int runsToRemove = runs - RunsShown;
+        int lastLineToRemove = 0;
+
+        int occurences = 0;
+        for (int i = 0; i < logLines.Length; ++i)
+        {
+            if (logLines[i] == RunSeparatorLine)
+                ++occurences;
+
+            if (occurences == runsToRemove + 1)
+            {
+                lastLineToRemove = i;
+                break;
+            }   
+        }
+
+        var formatted = logLines.Skip(lastLineToRemove);
+        Log($"Formatting log. Runs in log: {runs} | runs to remove: {runsToRemove}", false);
+        File.WriteAllLines(LogPath, formatted);
     }
     public static void Log(object msg, bool toFile = true)
     {
