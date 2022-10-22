89199cfd0525dc4cbad95c3353fae28f0a8318e2
Matěj Pešl <matejpesl1@gmail.com>
2022-10-22 13:16:12 +0200

    Change neutral resource language (en-US) -> (en-EN)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Matěj Pešl 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: NeutralResourcesLanguage("en-EN")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
